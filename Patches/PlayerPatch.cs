ca6eb830bcad0c7b874ca3e250e2c0de45f5d2cf
Extrudeous-101 <330390451+Extrudeous-101@users.noreply.github.com>
2026-09-17 20:57:28 +0300

    Wrapped PlayerPatch to try/catch

--- Patches/PlayerPatch.cs
+++ Patches/PlayerPatch.cs
@@ -1,3 +1,4 @@
+using System;
 using HarmonyLib;
 using UnityEngine;
 
@@ -18,19 +19,28 @@ namespace Extrudeous.DragnSprint.Patches
         private static float _originalState;
         static bool Prefix(T __instance)
         {
-            if (!DragnSpringMod.Singleton._sprint.Value)
+            try
             {
-                if (_originalState != 0f) walkingMovementSpeedRef(__instance) = _originalState;
-                _originalState = 0f;
-                return true;
-            }
+                if (!DragnSpringMod.Singleton._sprint.Value)
+                {
+                    if (_originalState != 0f) walkingMovementSpeedRef(__instance) = _originalState;
+                    _originalState = 0f;
+                    DragnSpringMod.ManualLog.LogDebug($"Changed speed to {walkingMovementSpeedRef(__instance)}.");
+                    return true;
+                }
 
-            if (_originalState == 0f)
-            {
+                if (_originalState != 0f) return true;
+                
                 _originalState = walkingMovementSpeedRef(__instance);
                 walkingMovementSpeedRef(__instance) = _originalState * DragnSpringMod.Singleton.SprintSpeed.Value;
+                DragnSpringMod.ManualLog.LogDebug($"Changed speed to {walkingMovementSpeedRef(__instance)}.");
             }
-            
+            catch (Exception ex)
+            {
+                DragnSpringMod.ManualLog.LogError($"Patch {typeof(PlayerPatch)} failed to prefix method.");
+                DragnSpringMod.ManualLog.LogError(ex);
+            }
+
             return true;
         }
     }
